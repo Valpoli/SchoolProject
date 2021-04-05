98531e7147c2f3c2dfad2fb6f9fd6eaa6a4a3e82
Valentin Poli <valentin.poli@epita.fr>
2021-04-05 18:48:19 +0100

    first commit

---
+++ VirtualGlobalCollege/VirtualGlobalCollege/Data/VirtualGlobalCollegeContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VirtualGlobalCollege.Models;
+
+namespace VirtualGlobalCollege.Data
+{
+    public class VirtualGlobalCollegeContext : DbContext
+    {
+        public VirtualGlobalCollegeContext(DbContextOptions<VirtualGlobalCollegeContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Student { get; set; }
+    }
+}
---
+++ VirtualGlobalCollege/VirtualGlobalCollege/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VirtualGlobalCollege.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Mail { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+}
